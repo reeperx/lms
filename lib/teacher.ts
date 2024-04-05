76e3f08e092c101e78d8ef33fe3c5e5c02b4a68d
reeperx10 <acebany080@gmail.com>
2024-04-05 10:04:00 +0200

    second changes

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,8 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+  // Assuming TEACHER_IDS is a comma-separated string of IDs
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_IDS 
+    ? process.env.NEXT_PUBLIC_TEACHER_IDS.split(',') 
+    : [];
+
+  return userId ? teacherIds.includes(userId) : false;
 }
